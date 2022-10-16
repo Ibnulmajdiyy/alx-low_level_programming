d03ff50ab79b8270052e2575834f4f84d85c70ed
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-16 04:16:52 -0700

    good

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int c;
+    /*scanf("%c", &c);*/
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+	return (0);
+}
