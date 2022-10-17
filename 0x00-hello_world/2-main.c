5b7fa089f00daf8786e323fc6ac47cf4191fa220
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-17 06:59:40 -0700

    too

---
+++ 0x00-hello_world/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Hello World!\n");
+	return (0);
+}
