be1d6cdb742329d5457518ac3e548d22d3bc505f
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-17 13:12:39 -0700

    food

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,10 @@
 
 int main(void)
 {
-	printf("Size of int: %lu bytes\n", sizeof(int));
-	printf("Size of float: %lu bytes\n", sizeof(float));
-	printf("Size of double: %lu bytes\n", sizeof(double));
 	printf("Size of char: %lu byte\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(double));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(double));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
