3f9a688aedaed179624b213f7d21ddb888755873
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-17 07:24:27 -0700

    to

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+	return (0);
+}
