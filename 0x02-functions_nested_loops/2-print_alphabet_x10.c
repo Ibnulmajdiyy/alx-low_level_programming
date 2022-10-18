9b2c9511c87fd1e7e71fba8eb0f3409bc20711f9
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-18 15:23:27 -0700

    Allah

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	char x;
+
+	while (count++ <= 9)
+	{
+	for (x = 'a'; x <= 'z'; ++x)
+		_putchar(x);
+	_putchar('\n');
+	}
+}
