4fa9bd51f02ba5f7ed02ea3d97d924d0b7539754
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-18 14:06:45 -0700

    sallalahualaihiwasallam

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -18,6 +18,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
