1ac9b1662a3791612eee4d891136d2c5f6ebef91
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-18 18:11:59 -0700

    betty 7-print_last_digit.c
    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 7-print_last_digit.c -o 7-print_last_digit.o
    /
    
    git commit -m

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,6 +13,15 @@ int print_last_digit(int n)
 	int digit;
 
 	digit = n % 10;
+	if (n > 0)
+	{
+
 		_putchar('0' + digit);
 		return (digit);
+	}
+	else
+	{digit = n % 10;
+		_putchar('0' + digit);
+	return (digit);
+	}
 }
