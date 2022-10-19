1b32be088e9a190caf9db22a83598c04256d04f0
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-18 17:04:49 -0700

    h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @n: int in question
+ *
+ * Return: abs of the int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
