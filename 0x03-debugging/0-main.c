a5acabc96855aee54a9ce2ae7001d94bfe61c9f4
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-20 06:28:30 -0700

    gerit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: 'the program's description'
- *
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
