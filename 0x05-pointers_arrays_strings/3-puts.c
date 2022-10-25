4f8b69e6046760da4894d1befd4fa37c80dd6f06
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-24 21:01:02 -0700

    Allah

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - check code
+ *
+ * @str: to print string
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
