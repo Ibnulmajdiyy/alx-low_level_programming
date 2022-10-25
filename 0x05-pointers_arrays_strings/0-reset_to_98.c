1cc57610fd6cc661dd94d9116a74c021ae2ecc82
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-24 19:38:06 -0700

    jip

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - to reset n to 98
+ * main - to reset n to 98
  *
- * @n: pointer variable
+ *
  * Return: 0
  */
 
-void reset_to_98(int *n)
+int main(void)
 {
 	int i;
+	int *n;
 
 	i = 402;
 	n = &i;
+
 	_putchar(i);
 
 	*n = 98;
+
 	_putchar(i);
+
+	return (0);
 
 
 
