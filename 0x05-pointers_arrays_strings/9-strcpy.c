2d51e91f02cece2d4650910d102405ffdc69f432
ibnulmajdiyy <ibnulmajdiy@gmail.com>
2022-10-24 21:57:09 -0700

    jjj

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,7 @@
  * _strcpy - check code
  *           terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
- * @src: The source string 
+ * @src: The source string
  *
  * Return: A pointer
  */
